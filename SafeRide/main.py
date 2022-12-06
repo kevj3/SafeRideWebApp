fd823de520b04242fdbca86c590ffd9552b37393
kevj3 <74331555+kevj3@users.noreply.github.com>
2022-12-05 19:43:47 -0700

    Add files via upload

--- SafeRide/main.py
+++ SafeRide/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy, query
 from flask_login import login_required, login_manager, login_user, logout_user, current_user, UserMixin, LoginManager
+import smtplib
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rides.db'
@@ -44,6 +45,23 @@ def home():
 
         db.session.add(ride)
         db.session.commit()
+
+        subject = "SafeRide"
+        text = "Hello " + name + " SafeRide has received your ride request." \
+                                 "You will receive a email when SafeRide accepts your ride request" \
+                                 "Hold on tight" \
+                                 "Thank you for being Safe!" \
+                                 "-SafeRide Gunnison"
+
+        message = 'Subject: {}\n\n{}'.format(subject, text)
+
+        server = smtplib.SMTP("smtp.office365.com", 587)
+        server.starttls()
+
+        server.login("SafeRideGunnison@outlook.com", "Safe81230")
+        server.sendmail("SafeRideGunnison@outlook.com", phone, message)
+        server.quit()
+
         return redirect('/confirmation/' + name)
     return render_template('home.html')
 
@@ -53,9 +71,10 @@ def rides():
     ride = Rides.query.all()
     return render_template("ride.html", ride=ride)
 
+
 @app.route('/confirmation/<name>')
 def confirm(name):
-    return render_template("confirmation.html",name=name)
+    return render_template("confirmation.html", name=name)
 
 
 @app.route('/ControlPanel')
@@ -64,12 +83,68 @@ def driver():
     ride = Rides.query.all()
     return render_template("driver.html", ride=ride)
 
-@app.route('/ControlPanel/<id>')
+
+@app.route('/ControlPanel/Complete/<id>')
 def delete(id):
     ride = Rides.query.filter_by(id=id).first()
     db.session.delete(ride)
     db.session.commit()
     return redirect(url_for('driver'))
+
+
+@app.route('/ControlPanel/Accept/<id>')
+def accept(id):
+    ride = Rides.query.filter_by(id=id).first()
+
+    email = ride.phone
+    name = ride.name
+
+    subject = "SafeRide"
+    text = "Hello " + name + " SafeRide has accepted your ride request expect a ride in 5 to 10 minutes." \
+                             "Thank you for choosing SafeRide!"
+    message = 'Subject: {}\n\n{}'.format(subject, text)
+
+    server = smtplib.SMTP("smtp.office365.com", 587)
+    server.starttls()
+
+    server.login("SafeRideGunnison@outlook.com", "Safe81230")
+    server.sendmail("SafeRideGunnison@outlook.com", email, message)
+    server.quit()
+    return render_template('current.html', ride=ride)
+
+
+@app.route('/ControlPanel/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        pickup = request.form['pick']
+        dropoff = request.form['drop']
+        comments = request.form['comment']
+        ride = Rides(name=name, phone=phone, pickup=pickup, dropoff=dropoff, comments=comments)
+
+        db.session.add(ride)
+        db.session.commit()
+
+        subject = "SafeRide"
+        text = "Hello " + name + " SafeRide has received your ride request." \
+                                 "You will receive a email when SafeRide accepts your ride request" \
+                                 "Hold on tight" \
+                                 "Thank you for being Safe!" \
+                                 "-SafeRide Gunnison"
+
+        message = 'Subject: {}\n\n{}'.format(subject, text)
+
+        server = smtplib.SMTP("smtp.office365.com", 587)
+        server.starttls()
+
+        server.login("SafeRideGunnison@outlook.com", "Safe81230")
+        server.sendmail("SafeRideGunnison@outlook.com", phone, message)
+        server.quit()
+
+        return redirect(url_for('driver'))
+    return render_template('add.html')
+
 
 @app.route('/logout')
 @login_required
@@ -79,7 +154,7 @@ def logout():
 
 
 @app.route('/AboutUs')
-def aboutus():
+def about():
     return render_template('about.html')
 
 
